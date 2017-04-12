7d29ef86b9061ff2e9e30e71472de692f987e0fd
gitmantis <kolesnikov.andrey@gmail.com>
2017-04-12 12:34:29 +0300

    добавлено использование getOnline

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,5 +1,5 @@
 import vk
-
+import getpass
 
 APP_ID = 5978862  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
@@ -17,21 +17,21 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope="friends"
     )
     api = vk.API(session)
-    friends_online = []
-    for friend in api.friends.get(fields='first_name,last_name,online'):
-        if int(friend["online"]) == 1:        
-            friends_online.append(friend)
-    return friends_online
+    friends_online = api.friends.getOnline(online_mobile=1)
+    return {"online": api.users.get(user_ids=','.join(map(str,friends_online["online"]))), "online_mobile": api.users.get(user_ids=','.join(map(str,friends_online["online_mobile"])))}
 
 def output_friends_to_console(friends_online):
-    for friend in friends_online:
+    pprint(friends_online)
+    for friend in friends_online["online"]:
         print("%s %s is online" % (friend["first_name"], friend["last_name"]))
-    #print(json.dumps(friends_online, indent=4, sort_keys=True, ensure_ascii=False))
+    for friend in friends_online["online_mobile"]:
+        print("%s %s is online mobile" % (friend["first_name"], friend["last_name"]))
 
 if __name__ == '__main__':
     login = get_user_login()
-    password = get_user_password()
+    password = getpass.getpass()
     friends_online = get_online_friends(login, password)
     output_friends_to_console(friends_online)
